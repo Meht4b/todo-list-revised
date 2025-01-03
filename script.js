52419aa47eb9d622c7acb985c26e74d2ce10ef09
Mehthab <mehthabunstoppable@gmail.com>
2025-01-03 22:59:05 +0530

    fixed checkbox bug

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-var checkbox = document.querySelector(".checkbox");
+var checkbox = document.querySelectorAll(".checkbox");
 
-
-checkbox.addEventListener('change', function() {
+for (i=0;i<checkbox.length;i++){
+checkbox[i].addEventListener('change', function() {
   if (this.checked) {
     this.nextSibling.nextElementSibling.style.display = 'flex';
   } else {
     this.nextSibling.nextElementSibling.style.display = 'none';
   }
-});
+});}
