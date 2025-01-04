b34a82275a9a45fc035d594610dacf97fa205c20
Mehthab <mehthabunstoppable@gmail.com>
2025-01-04 16:54:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -27,23 +27,35 @@ function submitFunction(){
     if (message != ''){
         document.getElementById('newTaskInput').value='';
         const liChild = document.createElement('li');
-        liChild.innerHTML = '<div class="newCheckbox"><input type="checkbox" class="checkbox"><div class="newCheckboxColor"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M20.285 2l-11.285 11.567-5.286-5.011-3.714 3.716 9 8.728 15-15.285z"/></svg></div></div>'
+        let checkboxEl = document.createElement('div');
+        checkboxEl.classList.add('newCheckbox');
+        let checkboxinput = document.createElement('input');
+        checkboxinput.type = 'checkbox';
+        checkboxinput.classList.add('checkbox');
+        let checkboxcolor = document.createElement('div');
+        checkboxcolor.innerHTML='<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M20.285 2l-11.285 11.567-5.286-5.011-3.714 3.716 9 8.728 15-15.285z"/></svg>';
+        checkboxcolor.classList.add('newCheckboxColor');
+
+        checkboxEl.appendChild(checkboxinput);
+        checkboxEl.appendChild(checkboxcolor)
 
         const senderMessage = document.createElement('h3');
         senderMessage.innerHTML=message;
 
-        
+        liChild.appendChild(checkboxEl);
         liChild.appendChild(senderMessage);
 
         
-        document.getElementById('taskList').appendChild(liChild);
+        document.getElementById('taskList').appendChild(liChild); 
 
-        console.log(document.querySelectorAll(".checkbox")[document.querySelectorAll(".checkbox").length-1].nextElementSibling);
+        checkboxinput.addEventListener('change',()=>{
+          if (checkboxinput.checked){
+            checkboxcolor.style.display = 'flex';
+          } else {
+            checkboxcolor.style.display = 'none';
+          }
+        })
 
-        var checkbox = document.querySelectorAll(".checkbox");
-        for (i=0;i<checkbox.length;i++){
-          checkbox[i].addEventListener('change',checkboxTick);
-        }
 
         
         /*
